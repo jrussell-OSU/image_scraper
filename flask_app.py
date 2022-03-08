b88d7c17674b39fbf1c74da84e0eeb46e741f45c
jrussell-OSU <russelj@oregonstate.edu>
2022-03-08 08:29:33 -0500

    Cleaned up some code smells

--- flask_app.py
+++ flask_app.py
@@ -7,7 +7,8 @@
 # this program uses thesaurus API to find synonyms and tries those.
 # See: README, if the program doesn't work. config.py must be created by user.
 
-# REFERENCES: https://www.geeksforgeeks.org/image-scraping-with-python/,  https://stackabuse.com/deploying-a-flask-application-to-heroku/,
+# REFERENCES: https://www.geeksforgeeks.org/image-scraping-with-python/,
+# https://stackabuse.com/deploying-a-flask-application-to-heroku/
 # thesaurus API is at https://words.bighugelabs.com/site/api
 
 
@@ -15,8 +16,10 @@ import requests
 from bs4 import BeautifulSoup
 import random
 from flask import Flask, jsonify, request
-import os
 from flask_cors import cross_origin
+# import config  # uncomment to run on localhost
+import os  # uncomment to run on Heroku
+
 
 app = Flask(__name__)
 
@@ -34,21 +37,21 @@ class ImageScraper:
         self._wiki_url = "https://commons.wikimedia.org/wiki/Category:" + self._word + "/json"
         self._synonym_url = ""
 
-    def retrieve_word(self):
-        word = input("Enter q to quit. \nGive me a word: ")
-        if word == "q":
-            print("Exiting...")
-            exit()
-        self.set_word(word)
-
     def set_word(self, word):
+        """Set word to find images for"""
         self._word = word
         self.set_wiki_url()
 
     def set_synonym_url(self):
+        """
+        Uncomment first line if running on Heroku
+        Uncomment second line if running on localhost
+        """
         self._synonym_url = "https://words.bighugelabs.com/api/2/" + os.environ["api_key"] + self._word + "/json"
+        # self._synonym_url = "https://words.bighugelabs.com/api/2/" + config.api_key + self._word + "/json"
 
     def set_wiki_url(self):
+        """Adds word to wiki url to find images related to that word"""
         self._wiki_url = "https://commons.wikimedia.org/wiki/Category:" + self._word
 
     def retrieve_page_data(self):
@@ -77,7 +80,7 @@ class ImageScraper:
                         fixed_url = temp[0:p]
                         self._valid_image_urls.append(fixed_url)
 
-    def retrieve_synonyms(self,):
+    def retrieve_synonyms(self):
         """
         Takes a word and uses API to get list of synonyms
         Thesaurus API: https://words.bighugelabs.com/site/api
@@ -85,20 +88,20 @@ class ImageScraper:
         self.set_synonym_url()
         try:
             # Use thesaurus API to get synonyms for the words
-            # response = requests.get(api_url + api_key + self._word + "/json").json()
             response = requests.get(self._synonym_url).json()
+            words = None
             word_types = [t for t in response]  # gets all word types for this word (e.g. noun, adjective, verb)
             for word_type in word_types:  # iterates through all words from all word types
                 words = response[word_type]['syn']  # ['syn'] is where synonyms stored (vs ['ant'] for antonym)
-                if words:
-                    for word in words:
-                        self._synonyms.append(word)
-                else:
-                    print("No synonyms found.")
+                for word in words:
+                    self._synonyms.append(word)
+            if not words:
+                print("No synonyms found.")
         except:
-            print("invalid json response from thesaurus api")
+            print("Invalid JSON response from thesaurus API.")
 
     def get_random_valid_image(self):
+        """Returns random url from a list of urls"""
         i = random.randint(0, len(self._valid_image_urls) - 1)
         return self._valid_image_urls[i]
 
@@ -107,22 +110,22 @@ class ImageScraper:
         Try to find valid image urls using synonyms of original word
         Return True if one is found, otherwise False
         """
-        self.retrieve_synonyms()
+        self.retrieve_synonyms()  # get word's synonyms from thesaurus API
         if not self._synonyms:
-            # print("No synonyms found.")
+            print("No synonyms found.")
             return False
         count = 0
         while not self._valid_image_urls and count < len(self._synonyms):
             self._word = self._synonyms[count]
+            self.image_scraper(self._word)
             print("Trying synonym: ", self._word)  # for DEBUGGING
-            self.set_wiki_url()
-            self.retrieve_page_data()
-            self.raw_image_urls()
-            self.valid_image_urls()
+            # self.set_wiki_url()
+            # self.retrieve_page_data()
+            # self.raw_image_urls()
+            # self.valid_image_urls()
             count += 1
         # If we have tried every synonym but still couldn't find a valid image
         if count >= len(self._synonyms):
-            # print("No results found.")
             return False
         return True
 
@@ -133,6 +136,15 @@ class ImageScraper:
         else:
             return False
 
+    def image_scraper(self, word):
+        """
+        Main image scraper function.
+        """
+        self.set_word(word)  # set the keyword
+        self.retrieve_page_data()  # get the wikimedia page data
+        self.raw_image_urls()  # get the raw image urls from the wikimedia page
+        self.valid_image_urls()  # turn those raw urls into useable full-size image urls
+
 
 @app.route('/')
 def index():
@@ -142,32 +154,25 @@ def index():
 
 # Reference: https://stackabuse.com/deploying-a-flask-application-to-heroku/
 @app.route('/get_image_url/', methods=['GET'])
-@cross_origin()
+@cross_origin()  # for CORS
 def respond():
-    scraper = ImageScraper()
 
     # extract the word from the url
     word = request.args.get("word", None)
-
     print("Word received: ", word)
 
-    response = {}
-
-    response["IMAGE_URL"] = "ERROR"
+    # Set response as Error by default until image URL found
+    response = {"IMAGE_URL": "ERROR"}
 
     # Verify if a word was received
     if not word:
         return jsonify(response)
 
     scraper = ImageScraper()
-    scraper.set_word(word)
-    scraper.retrieve_page_data()
-    scraper.raw_image_urls()
-    scraper.valid_image_urls()
+    scraper.image_scraper(word)  # Main function
 
     # If no valid image urls found, try synonyms
     if not scraper.check_valid_image_urls():
-        # print("No results, trying synonyms...")
         if not scraper.try_synonyms():
             print("No results using synonyms")
             return jsonify(response)
@@ -176,7 +181,6 @@ def respond():
     image_url = scraper.get_random_valid_image()
     if image_url:
         response["IMAGE_URL"] = image_url
-
     return jsonify(response)
 
 
